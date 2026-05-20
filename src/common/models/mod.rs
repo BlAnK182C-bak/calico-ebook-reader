270945c713ad4620957d303665cb40ccf56fb588
Abhinav Kumar Singh <abhinav.30.kr.linux.bak@gmail.com>
2026-05-20 14:55:18 +0530

    Feat/reading from bookmap by default (#34)
    * refactor: added filepath to bookmap and moved bookmarks and bookmap to settings
    
    * feat: added filetype to bookmap
    
    * feat: scanning bookmap before sources

--- src/common/models/mod.rs
+++ src/common/models/mod.rs
@@ -1,5 +1,4 @@
 pub(crate) mod book;
-pub(crate) mod bookmarks;
 pub(crate) mod filetypes;
 pub(crate) mod line;
 pub(crate) mod settings;
--- src/common/models/settings.rs
+++ src/common/models/settings.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use uuid::Uuid;
 
+use crate::common::{constants::BOOKMARKS_FILE_PATH, models::filetypes::BookFileTypes};
 #[derive(Deserialize, Serialize)]
 pub(crate) struct Settings {
     sources: SourceSettings,
@@ -8,6 +11,25 @@ pub(crate) struct Settings {
 #[derive(Deserialize, Serialize)]
 pub(crate) struct SourceSettings {
     source_paths: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize, Default)]
+pub(crate) struct Bookmarks {
+    bookmarks: HashMap<String, BookBookmark>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct BookBookmark {
+    offset: usize,
+    last_read: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct BookMap<'a> {
+    uuid: Uuid,
+    filename: &'a str,
+    filepath: &'a str,
+    filetype: &'a str,
 }
 
 impl Settings {
@@ -24,5 +46,72 @@ impl Settings {
 impl SourceSettings {
     pub(crate) fn new(source_paths: Vec<String>) -> Self {
         Self { source_paths }
+    }
+}
+
+impl Bookmarks {
+    pub(crate) fn load_bookmarks(&self) -> Result<Self, std::io::Error> {
+        let contents = std::fs::read_to_string(BOOKMARKS_FILE_PATH.to_path_buf())?;
+        if contents.is_empty() {
+            Ok(Bookmarks::default())
+        } else {
+            toml::from_str(&contents).map_err(|e| std::io::Error::other(e))
+        }
+    }
+
+    pub(crate) fn get_bookmarks(&self) -> &HashMap<String, BookBookmark> {
+        &self.bookmarks
+    }
+
+    pub(crate) fn set_bookmarks(
+        &mut self,
+        book_id: &str,
+        offset: usize,
+    ) -> Result<(), std::io::Error> {
+        self.bookmarks.insert(
+            book_id.into(),
+            BookBookmark {
+                offset,
+                last_read: std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap_or_default()
+                    .as_secs() as usize,
+            },
+        );
+
+        let contents = toml::to_string(self).map_err(|e| std::io::Error::other(e))?;
+        std::fs::write(BOOKMARKS_FILE_PATH.to_path_buf(), contents)
+    }
+}
+
+impl BookBookmark {
+    pub(crate) fn get_offset(&self) -> usize {
+        self.offset
+    }
+}
+
+impl<'a> BookMap<'a> {
+    pub(crate) fn new(uuid: Uuid, filename: &'a str, filepath: &'a str, filetype: &'a str) -> Self {
+        Self {
+            uuid,
+            filename,
+            filepath,
+            filetype,
+        }
+    }
+    pub(crate) fn get_uuid(&self) -> Uuid {
+        self.uuid
+    }
+
+    pub(crate) fn get_filename(&self) -> &str {
+        &self.filename
+    }
+
+    pub(crate) fn get_filepath(&self) -> &str {
+        self.filepath
+    }
+
+    pub(crate) fn get_filetype(&self) -> BookFileTypes {
+        BookFileTypes::new(self.filetype)
     }
 }
