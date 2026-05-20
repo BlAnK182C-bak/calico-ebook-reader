9b4d3c1e6b56a56fc7921b0473bbe1f94beef77f
Abhinav Kumar Singh <abhinav.30.kr.linux.bak@gmail.com>
2026-05-20 15:05:16 +0530

    Revert "Feat/reading from bookmap by default (#34)" (#35)
    This reverts commit 270945c713ad4620957d303665cb40ccf56fb588.

--- src/common/utils/settings.rs
+++ src/common/utils/settings.rs
@@ -1,32 +1,15 @@
 use std::{collections::HashMap, fs};
 
 use crate::common::{
-    constants::{BOOKMAP_FILE_PATH, SETTINGS_FILE_PATH},
-    models::{
-        filetypes::BookFileTypes,
-        settings::{BookMap, Settings},
-    },
+    constants::SETTINGS_FILE_PATH,
+    models::{filetypes::BookFileTypes, settings::Settings},
 };
 
 pub(crate) fn scan_sources_for_books()
 -> Result<HashMap<String, BookFileTypes>, Box<dyn std::error::Error>> {
-    let mut all_books: HashMap<String, BookFileTypes> = if BOOKMAP_FILE_PATH.exists() {
-        let bookmap_content = fs::read_to_string(BOOKMAP_FILE_PATH.to_path_buf())?;
-        if bookmap_content.trim().is_empty() {
-            HashMap::new()
-        } else {
-            let res: Vec<BookMap> = serde_json::from_str(&bookmap_content)?;
-            res.into_iter()
-                .map(|item| (String::from(item.get_filepath()), item.get_filetype()))
-                .collect()
-        }
-    } else {
-        HashMap::new()
-    };
-
-    // scan for books in source settings
     let settings_file_content = fs::read_to_string(SETTINGS_FILE_PATH.to_path_buf())?;
     let settings: Settings = toml::from_str(&settings_file_content)?;
+    let mut all_books: HashMap<String, BookFileTypes> = HashMap::new();
     for source in settings.get_get_source_paths().iter() {
         for entry in fs::read_dir(source)? {
             let entry = entry?;
@@ -34,14 +17,6 @@ pub(crate) fn scan_sources_for_books()
 
             if path.is_file() {
                 let stringified_path = path.to_string_lossy().to_string();
-
-                if all_books.contains_key(&stringified_path) {
-                    println!(
-                        "scan_sources_for_books: Skipping already extracted: {}",
-                        stringified_path
-                    );
-                    continue;
-                }
                 let Some(extension) = path
                     .extension()
                     .and_then(|e| e.to_str().filter(|e| !e.is_empty()))
