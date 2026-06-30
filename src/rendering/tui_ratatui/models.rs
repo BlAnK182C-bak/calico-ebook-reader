a95d6dd094599ca44f1710fee453fe1921bca825
Abhinav Kumar Singh <abhinav.30.kr.linux.bak@gmail.com>
2026-06-30 13:31:15 +0530

    bugfix (tui): Added scroll with list state instead of current book index

--- src/rendering/tui_ratatui/models.rs
+++ src/rendering/tui_ratatui/models.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, io::Stdout};
 
-use ratatui::{Terminal, prelude::CrosstermBackend};
+use ratatui::{Terminal, prelude::CrosstermBackend, widgets::ListState};
 
 use crate::{common::models::book::Book, pagination::models::Page, rendering::models::AppState};
 
@@ -11,7 +11,7 @@ pub(crate) struct RatatuiApp<'a> {
     pub(super) books: &'a [Book],
     pub(super) curr_book_pages: Option<Vec<Page>>,
     pub(super) curr_book_lookup: Option<HashMap<usize, usize>>,
-    pub(super) curr_book_idx: usize,
+    pub(super) list_state: ListState,
 
     pub(super) byte_offset: usize,
     pub(super) should_quit: bool,
